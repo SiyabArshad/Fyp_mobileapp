7ed0b9f55c8435bf0e9244ec676acd4657d694ce
Siyab Arshad <siyabarshadsatti@gmail.com>
2023-04-15 23:42:01 +0500

    reult modal added

--- src/Screens/Results.jsx
+++ src/Screens/Results.jsx
@@ -5,77 +5,14 @@ import fonts from "../configs/fonts"
 import colors from '../configs/colors'
 import { RFPercentage as rp, RFValue as rf } from "react-native-responsive-fontsize";
 import IonicIcon from 'react-native-vector-icons/Ionicons';
-import * as FileSystem from 'expo-file-system';
-const { StorageAccessFramework } = FileSystem;
 import Loading from "../Components/Loading"
+import ResultModal from '../Components/ResultModal';
 export default function Results({navigation}) {
     const [isload,setisload]=React.useState(false)
     const [isVisible, setIsVisible] = React.useState(false);
-    const [downloadProgress, setDownloadProgress] = React.useState();
-    const downloadPath = FileSystem.documentDirectory + (Platform.OS == 'android' ? '' : '');
-    const ensureDirAsync = async (dir, intermediates = true) => {
-      const props = await FileSystem.getInfoAsync(dir)
-      if (props.exist && props.isDirectory) {
-          return props;
-      }
-      let _ = await FileSystem.makeDirectoryAsync(dir, { intermediates })
-      return await ensureDirAsync(dir, intermediates)
+  const clsoemodal=()=>{
+    setIsVisible(false)
   }
-  const downloadCallback = downloadProgress => {
-    const progress = downloadProgress.totalBytesWritten / downloadProgress.totalBytesExpectedToWrite;
-    setDownloadProgress(progress);
-};
-const downloadFile = async (fileUrl) => {
-  if (Platform.OS == 'android') {
-    const dir = ensureDirAsync(downloadPath);
-  }
-
-  let fileName = fileUrl.split('Result/')[1];
-  //alert(fileName)
-  const downloadResumable = FileSystem.createDownloadResumable(
-    fileUrl,
-    downloadPath + fileName,
-    {},
-    downloadCallback
-  );
-  const saveAndroidFile = async (fileUri, fileName = 'File') => {
-    try {
-      const fileString = await FileSystem.readAsStringAsync(fileUri, { encoding: FileSystem.EncodingType.Base64 });
-      
-      const permissions = await StorageAccessFramework.requestDirectoryPermissionsAsync();
-      if (!permissions.granted) {
-        return;
-      }
-
-      try {
-        await StorageAccessFramework.createFileAsync(permissions.directoryUri, fileName, 'application/pdf')
-          .then(async (uri) => {
-            await FileSystem.writeAsStringAsync(uri, fileString, { encoding: FileSystem.EncodingType.Base64 });
-            alert('Report Downloaded Successfully')
-          })
-          .catch((e) => {
-          });
-      } catch (e) {
-        throw new Error(e);
-      }
-
-    } catch (err) {
-    }
-  }
-  try {
-    const { uri } = await downloadResumable.downloadAsync();
-    if (Platform.OS == 'android')
-      saveAndroidFile(uri, fileName)
-    else
-      saveIosFile(uri);
-  } catch (e) {
-    console.error('download error:', e);
-  }
-}
-const saveIosFile = (fileUri) => {
-  // your ios code
-  // i use expo share module to save ios file
-}
   React.useEffect(()=>{
     setisload(true)
     setTimeout(() => {
@@ -85,6 +22,7 @@ const saveIosFile = (fileUri) => {
   return (
     <View style={styles.mnonb}>
       <Loading visible={isload}/>
+      <ResultModal showmodal={clsoemodal} visible={isVisible}/>
 <View style={{display:"flex",flexDirection:"row",justifyContent:"space-between",alignItems:"center",marginTop:rp(5)}}>
    <Text style={{fontSize:rp(5),fontFamily:fonts.Nextrabold}}>DIGI SCHOOL!</Text>
 </View>
@@ -97,7 +35,7 @@ const saveIosFile = (fileUri) => {
     [1,2,3,4,5].map((item,i)=>(
         <View style={{borderWidth:1,borderColor:colors.green,marginBottom:rp(1),paddingHorizontal:rp(2),paddingVertical:rp(1.7),borderRadius:rp(1),display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"space-between"}}>
             <Text style={{color:colors.black,fontFamily:fonts.Nregular}}>Mid Term Results</Text>
-            <TouchableOpacity onPress={()=>downloadFile("https://drive.google.com/file/d/1JR06fL6UVSBqlXAfWBFVnfJRr6pCLVnv/view?usp=drivesdk")}>
+            <TouchableOpacity onPress={()=>setIsVisible(true)}>
             <IonicIcon name='download' size={28} color={colors.green}/>
             </TouchableOpacity>
         </View>
